a3e38d1bad26470708c4f704411f6e4999742d4d
Lucas Joannes <joannesribeiro_@outlook.com>
2024-03-23 15:00:08 -0300

    feat: Finalizado lib para gerar senhas

--- libs/generator_password.py
+++ libs/generator_password.py
@@ -4,48 +4,59 @@ import os
 import time
 import getpass
 
-class PassGen():
+
+class PassGen:
     def __init__(self) -> None:
-        self.password_length = None 
+        self.password_length = None
 
     def get_length_password(self):
-        self.password_length = int(getpass.getpass("Please enter the number of characters you want in your password.\n"))
-             
+        self.password_length = int(
+            getpass.getpass("Please enter a number of 8 to 14 characters.\n")
+        )
+
         if self.password_length > 14:
-            print("Enter a password that is 14 characters or less. You don't want to crash the program, right?")
-            time.sleep(5)
-            os.system('clear')
+            print(
+                "Enter a password that is 8 to 14 characters. You don't want to crash the program, right?"
+            )
+            time.sleep(2)
+            os.system("clear")
             return self.get_length_password()
         elif self.password_length < 8:
             print("For security, please enter a longer password")
-            time.sleep(5)
-            os.system('clear')
+            time.sleep(2)
+            os.system("clear")
             return self.get_length_password()
-        
+
     def get_random_string(self):
         lowercase_letters = string.ascii_lowercase
         uppercase_letters = string.ascii_uppercase
         numbers = string.digits
         especial_strings = string.punctuation
 
-        all_characters = lowercase_letters + uppercase_letters + numbers + especial_strings
-        self.result_password = ''.join(random.choice(all_characters) for _ in range(self.password_length))
-        self.password_generated = self.result_password.replace(self.result_password, "***********")
-        
+        all_characters = (
+            lowercase_letters + uppercase_letters + numbers + especial_strings
+        )
+        self.result_password = "".join(
+            random.choice(all_characters) for _ in range(self.password_length)
+        )
+        self.password_generated = self.result_password.replace(
+            self.result_password, "***********"
+        )
+
     def interation_with_user(self):
         print("Password generated is sucessful:", self.password_generated, "\n")
         print("You wish see your password?")
         print("Press 1, for yes...")
         print("Press 2, for exit...")
-        
-        user_response = int(input("Option:"))
+
+        user_response = int(input("Option: "))
         print("\n")
         if user_response == 1:
             print(self.result_password)
         else:
             print("Exiting for system...")
-            time.sleep(4)
-            os.system('clear')
+            time.sleep(3)
+            os.system("clear")
             pass
 
 
